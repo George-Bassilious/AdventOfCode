d32cbf98591179321375135d3d41a695614e66e2
George <george.bassilious@gmail.com>
2020-12-03 23:48:15 +0100

    day2 complete

--- 2020/2/day2.py
+++ 2020/2/day2.py
@@ -11,18 +11,33 @@ for line in file1.readlines():
 def solution1():
     count=0
     for [n1,n2,c,word] in numbersList:
-        diff=abs(int(n2)-int(n1))
+
         occur =word.count(c)
-        if occur > int(n1) and occur < int(n2):
+       
+        if occur >= int(n1) and occur <= int(n2):
+           # print("%s-%s"%(n1,n2))
+         #   print ("%s in word : %s has count %d"%(c,word,occur))
+          #  print(occur)
             count=count+1
-    
 
-        
     return count
-       # print(string,string2,string3[0])
 
+def solution2():
+    count=0
+    for [n1,n2,c,word] in numbersList:
 
+        valid1= word[int(n1)-1] == c and c != word[int(n2)-1]
+        valid1s= word[int(n1)-1] != c and c == word[int(n2)-1]
+        valid2= word[int(n1)-1] != c and c != word[int(n2)-1]
+        valid3= word[int(n1)-1] == c and c == word[int(n2)-1]
+        
+       
+        if valid1 or valid1s and not (valid2 and valid3):
+            count=count+1
+
+    return count
 
 
 if __name__ == '__main__':
  print(solution1())
+ print(solution2())
