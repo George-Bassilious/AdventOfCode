86a744e2f84f17d5bfaee9cc60e80f64786a35ab
George <george.bassilious@gmail.com>
2020-12-05 01:49:15 +0100

    day 3 complete

--- 2020/3/day3.py
+++ 2020/3/day3.py
@@ -1,39 +1,33 @@
 file1 = open('input', 'r') 
-
 test1=file1.readlines()
 
 
-
-def solution3():
+def treeFinder(x,y):
     count=0
-    count2=0
-    playerPos= (0,0)
+    playerpos=y
+    length= len(test1[0])-1
+    for line in range(x,len(test1),x):
+    
+        if '#' in test1[line][(playerpos%length)]:
+            count=count+1
+        playerpos= playerpos+y
 
-    move = (3,1)
-    results=[]
-    for x in range(len(test1)):
-        length= len(test1[1])
-        print(1+x)
-        print((((1+x)*3))%length)
-        
-        if '#' in test1[(1+x)%length][(((1+x)*3))%length]:
-            count2=count2+1
-       
-        results.append(test1[(1+x)%length][(((1+x)*3))%length])
-      
-
-    print(len(results))
-
-    print(count2)
-    count= results.count('#')
-    print(count)
-   
     return count
 
-    
-    
 
 
+def solution1():
+    return treeFinder(1,3)
+
+def solution2():
+    count=1
+    slopes=[(1,1),(1,3),(1,5),(1,7),(2,1)]
+    for (x,y) in slopes:
+        count*= treeFinder(x,y)
+    
+    return count
 
 if __name__ == '__main__':
-    print(solution3())
+    print(solution1())
+    print(solution2())
+  
