a0b787d915d4d99ee83bfe3042acfdf1ad080b74
George <george.bassilious@gmail.com>
2020-12-09 03:28:41 +0100

    minor improtvements

--- 2020/5/day5.py
+++ 2020/5/day5.py
@@ -4,9 +4,8 @@ inputData = open('input', 'r').read().splitlines()
 boardingPasses=[]
 
 def Calculate_seat_id(input):
-    a = list(range(127+1))
-    b= list(range(7+1))
-
+    a = list(range(128))
+    b= list(range(8))
     for l in input:
         if l == 'F':
            a= a[:int(len(a)/2)]
@@ -16,10 +15,8 @@ def Calculate_seat_id(input):
             b=b[int(len(b)/2):]
         if l== 'L':
             b=b[:int(len(b)/2)]
-        
-    input = (a[0],b[0])
 
-    return calculate_id(input)
+    return calculate_id((a[0],b[0]))
 
 def calculate_id(input):
     (row,col)=input
@@ -32,10 +29,8 @@ def solution1():
     return max(boardingPasses)
 
 def solution2():
-    diff=list(set(range(905))-set(boardingPasses))
+    diff=list(set(range(solution1()))-set(boardingPasses))
     return diff[-1:][0]
-
-
 
 
 if __name__ == '__main__':
