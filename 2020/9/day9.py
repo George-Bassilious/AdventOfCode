5d4f746509abd3686d6c219b851d1ac9240fd872
George <george.bassilious@gmail.com>
2020-12-10 03:55:59 +0100

    one star working, not so efficient

---
+++ 2020/9/day9.py
@@ -0,0 +1,23 @@
+import itertools
+
+
+def solution1():
+    with open("input") as f:
+        list1= [int(f.readline()) for i in range(25)]
+        n = int(f.readline())
+        
+        while n in ([x+y for (x,y) in list((itertools.product(list1,repeat=2))) if x!=y]):
+            list1.pop(0)
+            list1.append(n)
+            
+            n = int(f.readline())
+           
+
+        return n
+
+
+if __name__ == '__main__':
+    print(solution1())
+    #print(solution2())
+
+    
