becf6ee8779f353d1ece32102821a4b44f905e98
George <george.bassilious@gmail.com>
2020-12-11 01:11:22 +0100

    day 9 sol2 comp

--- 2020/9/day9.py
+++ 2020/9/day9.py
@@ -5,17 +5,43 @@ def solution1():
     with open("input") as f:
         list1= [int(f.readline()) for i in range(25)]
         n = int(f.readline())
-        
+        datae=[]+list1
+        datae.append(n)
+
         while n in ([x+y for (x,y) in list((itertools.product(list1,repeat=2))) if x!=y]):
             list1.pop(0)
-            list1.append(n)   
+            list1.append(n)  
+            
             n = int(f.readline())
+            datae.append(n) 
 
-        return n
+        return datae
 
+def solution2(data):  
+    subset=subsetsum(data, data[-1])
+    return min(subset)+max(subset)
+
+#inspired from online site about pointer chasing algorithms
+def subsetsum(arr,num): 
+    n=len(arr)
+    for i in range(n): 
+        curr_sum = arr[i] 
+        j = i + 1
+        while j <= n:          
+            if curr_sum == num: 
+                return arr[i:j-1] 
+      
+            if curr_sum > num or j == n: 
+                break
+              
+            curr_sum = curr_sum + arr[j] 
+            j += 1
+  
+    print ("No subarray found") 
+    return []
+    
 
 if __name__ == '__main__':
-    print(solution1())
-    #print(solution2())
-
-    
+    x= solution1()
+    print(x[-1])
+    print(solution2(x))
